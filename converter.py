87951895d1b858f96eeea17cc22c3c5f87a2ae82
Luc1an0s <lucianomaciel53@gmail.com>
2025-09-19 14:46:31 -0400

    update

--- converter.py
+++ converter.py
@@ -1,7 +1,6 @@
 from PIL import Image
 
-# Abre e converte para RGB puro
+
 img = Image.open("luciano.jpg").convert("RGB")
 
-# Salva como nova imagem limpa
 img.save("luciano_clean.jpg", format="JPEG")
--- teste.py
+++ teste.py
@@ -1,14 +1,14 @@
 import cv2
 import face_recognition
 
-# Carrega a imagem
+
 image = cv2.imread("luciano.jpg")
 rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-# Detecta landmarks
+
 landmarks_list = face_recognition.face_landmarks(rgb_image)
 
-# Desenha os pontos faciais
+
 for landmarks in landmarks_list:
     for feature, points in landmarks.items():
         for i in range(len(points) - 1):
@@ -16,7 +16,7 @@ for landmarks in landmarks_list:
             pt2 = points[i + 1]
             cv2.line(image, pt1, pt2, (0, 255, 0), 2)
 
-# Exibe a imagem com linhas faciais
+
 cv2.imshow("Linhas do Rosto", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
